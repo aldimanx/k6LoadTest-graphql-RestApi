f42c5789af5cd790b630c368e408c03606d6999a
aldiansyahN-kargo <aldiansyah.noverio@kargo.tech>
2023-07-27 01:56:02 +0700

    oke

--- script/introduction.js
+++ script/introduction.js
@@ -1,11 +1,6 @@
 import http from 'k6/http';
 
-export const options = {
-  vus: 10,
-  duration: '5s',
-};
-
 export default function() {
 
-let test= http.get('https://pegipegi.com')
+http.get('https://pegipegi.com')
 }
